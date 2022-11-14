24ec3bb7cdcc644d5accda0e659854027e5fe90f
ShevtsovDenis <shevcov.organika@yandex.ru>
2022-11-14 23:56:31 +0300

    Добавьте файлы проекта.

---
+++ Самостоятельная работа 14. Абстрактные классы/Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Самостоятельная_работа_14.Абстрактные_классы
+{//создаем класс, который наследует архитектуру родительского класса
+    class Cat : Animal
+    {//создаем поля для названия и звука
+        string title;
+        string sound;
+        //переопределяем свойство названия 
+        public override string Title
+        {
+            get => title;
+            set => title = value;
+        }
+        //создаем конструктор, который обращается к базовому и добавляем инициализацию поля звук
+        public Cat(string title, string sound)
+            :base()
+        {
+            this.sound = sound;
+        }
+        //переопределяем абстрактный метод, который выводит звук 
+        public override void GetSound()
+        {
+            Console.WriteLine(sound);
+        }
+    }
+}
---
+++ Самостоятельная работа 14. Абстрактные классы/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Самостоятельная_работа_14.Абстрактные_классы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("123","говорит Мяу");
+            cat.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
